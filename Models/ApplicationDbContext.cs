ce78cf27954e8b96dea21e38dec9d6413be95628
HombreLaser <sebastian-440@live.com>
2022-11-17 20:28:08 -0600

    Añadida migración inicial corregida

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -5,7 +5,11 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BackendPIA.Models {
-    public class ApplicationDbContext : IdentityDbContext {
+    public class ApplicationDbContext : IdentityDbContext<UserAccount> {
 	    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            base.OnModelCreating(builder);
+        }
     }
 }
